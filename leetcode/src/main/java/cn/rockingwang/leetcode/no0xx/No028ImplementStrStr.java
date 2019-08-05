5d4b52c516310eb767a02de809eb6cc3eefa4aff
Rocking <rocking1216@gmail.com>
2019-08-05 18:46:47 +0800

    Leetcode028

--- leetcode/src/main/java/cn/rockingwang/leetcode/no0xx/No028ImplementStrStr.java
+++ leetcode/src/main/java/cn/rockingwang/leetcode/no0xx/No028ImplementStrStr.java
@@ -18,17 +18,32 @@ public class No028ImplementStrStr {
 
     public static int strStr(String haystack, String needle) {
         int startIndex = 0;
-        while (startIndex < haystack.length()) {
+        if (needle.length() == 0) {
+            return 0;
+        }
+        if (haystack.length() == 0 || needle.length() > haystack.length()) {
+            return -1;
+        }
+        while (startIndex < haystack.length() - needle.length() + 1) {
             if (haystack.charAt(startIndex) == needle.charAt(0)) {
+                boolean match = true;
                 for (int i = 0; i < needle.length(); i++) {
-                    if (haystack.charAt(startIndex + i) != )
+                    if (haystack.charAt(startIndex + i) != needle.charAt(i)) {
+                        match = false;
+                        break;
+                    }
+                }
+                if (match) {
+                    return startIndex;
                 }
             }
+            startIndex++;
         }
+        return -1;
     }
 
     public static void main(String[] args) {
-        int index = strStr("hello", "ll");
+        int index = strStr("mississippi", "issipi");
         System.out.println("index: " + index);
     }
 
